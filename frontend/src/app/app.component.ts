d21ed67546412f8eafee4e7e1b9d1b47261a8db8
Tambinos <Ibimsicecream@gmail.com>
2025-05-27 14:07:03 +0200

    Pocketbase setup and init

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -7,6 +7,7 @@ import {MatSlideToggleModule} from '@angular/material/slide-toggle';
 import {MatIconModule} from '@angular/material/icon';
 import {Router, RouterLink, RouterOutlet} from '@angular/router';
 import {MatCheckbox} from '@angular/material/checkbox';
+import {PocketBaseService} from './pocketbase/pocket-base.service';
 
 @Component({
   selector: 'app-root',
@@ -20,7 +21,6 @@ import {MatCheckbox} from '@angular/material/checkbox';
     MatSlideToggleModule,
     MatIconModule,
     RouterOutlet,
-    RouterLink,
     MatCheckbox
   ],
   templateUrl: './app.component.html',
@@ -30,15 +30,49 @@ export class AppComponent {
   isLoggedIn = false;
   username = '';
   password = '';
-  hidePassword = true; // For the password visibility toggle
-  register = false;
-  constructor(private router: Router) {
+  repeatedPassword = '';
+  hidePassword = true;
+  hideRepeatedPassword = true;
+  isRegister = false;
+
+  constructor(private router: Router, private pb: PocketBaseService) {
+    this.pb.isLoggedIn$.subscribe(isLoggedIn => {
+      this.isLoggedIn = isLoggedIn;
+      if (isLoggedIn) {
+        this.router.navigate(['/home']);
+      }
+    });
   }
 
   login() {
     if (this.username && this.password) {
-      this.router.navigate(['/home']);
-      this.isLoggedIn = true;
+      this.pb.signIn(this.username, this.password).subscribe({
+        next: (response) => {
+          console.log('Login successful:', response);
+          this.router.navigate(['/home']);
+          this.isLoggedIn = true;
+        },
+        error: (error) => {
+          alert('Login failed. Please check your credentials.');
+        }
+      });
+    }
+  }
+
+  register() {
+    if (this.username && this.password) {
+      this.pb.register(
+        this.username,
+        this.password,
+        this.password
+      ).subscribe({
+        next: (response) => {
+          this.login();
+        },
+        error: (error) => {
+          alert('Registration failed. Please check your details.');
+        }
+      });
     }
   }
 }
