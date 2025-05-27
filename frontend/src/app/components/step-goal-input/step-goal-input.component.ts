3e9a366c2313c1318500bf8afbd8b3e45e859025
mle146119 <mle146119@stud.gibb.ch>
2025-05-27 10:58:44 +0200

    remove useless imports

--- frontend/src/app/components/step-goal-input/step-goal-input.component.ts
+++ frontend/src/app/components/step-goal-input/step-goal-input.component.ts
@@ -1,30 +1,18 @@
-import { Component } from '@angular/core';
-import {MatCard, MatCardActions, MatCardContent, MatCardHeader} from '@angular/material/card';
+import {Component} from '@angular/core';
 import {MatButton} from '@angular/material/button';
 import {MatFormField, MatInput, MatLabel} from '@angular/material/input';
 import {FormsModule} from '@angular/forms';
-import {DatePipe, DecimalPipe, NgForOf, NgIf} from '@angular/common';
-import {MatIcon} from '@angular/material/icon';
 import {Router, RouterLink} from '@angular/router';
+
 @Component({
   selector: 'app-step-goal-input',
   standalone: true,
   imports: [
-    MatCardContent,
-    MatCard,
-    MatCardHeader,
-    MatIcon,
     MatFormField,
     MatLabel,
-    MatIcon,
     MatButton,
     MatInput,
     FormsModule,
-    NgForOf,
-    NgIf,
-    MatCardActions,
-    DatePipe,
-    DecimalPipe,
     RouterLink
   ],
   templateUrl: './step-goal-input.component.html',
@@ -33,7 +21,8 @@ import {Router, RouterLink} from '@angular/router';
 export class StepGoalInputComponent {
   manualSteps: number | null = null;
 
-  constructor(private router: Router) { }
+  constructor(private router: Router) {
+  }
 
   addStepGoal() {
     if (this.manualSteps && this.manualSteps > 0) {
